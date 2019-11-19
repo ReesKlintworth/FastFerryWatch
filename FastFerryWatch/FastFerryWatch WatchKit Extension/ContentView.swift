6e3cfbb038f49440bed174dad42baf37084b28d6
Rees Klintworth <rees.klintworth@gmail.com>
2019-11-19 21:12:34 +1100

    Stub out new watch project, get it building on my watch.

---
+++ FastFerryWatch/FastFerryWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastFerryWatch WatchKit Extension
+//
+//  Created by Rees Klintworth on 11/19/19.
+//  Copyright © 2019 Rees Klintworth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Fast Ferry Schedule!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastFerryWatch/FastFerryWatch WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  FastFerryWatch WatchKit Extension
+//
+//  Created by Rees Klintworth on 11/19/19.
+//  Copyright © 2019 Rees Klintworth. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
